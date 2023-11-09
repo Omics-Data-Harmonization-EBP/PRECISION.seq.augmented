edd0c65927179b319c277a8d98468ece633a9c9e
yilin123 <59527986+yilinwu123@users.noreply.github.com>
2023-11-09 19:57:20 +0800

    Update 01_S4_structure.R

--- R/01_S4_structure.R
+++ R/01_S4_structure.R
@@ -1,6 +1,12 @@
 precision <- methods::setClass(
   "precision",
   slots = c(
+    raw.train.data = "list",
+    raw.test.data = "list",
+    harmon.train.data = "list",
+    harmon.test.data = "list",
+    classification.train.result = "list",
+    classification.test.result = "list",
     raw.data = "list",
     harmon.data = "list",
     cluster.result = "list"
@@ -13,3 +19,10 @@ create_precision.cluster <- function(data, label) {
   return(object)
 }
 
+create_precision.classification <- function(traindata, testdata, trainlabel, testlabel) {
+  object <- methods::new(Class = "precision",
+                         raw.train.data = list(data = traindata, label = trainlabel),
+                         raw.test.data = list(data = testdata, label = testlabel)
+                         )
+  return(object)
+}
